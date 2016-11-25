62278dad79d83c136b912c1642fa8ecc74cc7dde
Jindrich Bartek <jindrich.bartek@gmail.com>
2016-11-26 00:31:26 +0100

    add date time utc preview

--- client/modules/core/components/form/dateTimePicker.js
+++ client/modules/core/components/form/dateTimePicker.js
@@ -7,20 +7,36 @@ import Datetime from 'react-datetime';
 
 class DateTimePicker extends t.form.Component { // extend the base class
 
+  constructor (props) {
+    super(props);
+    this.state = {
+      UTCRenderedDateTime: null
+    }
+  }
+
   getTemplate() {
+    //const utcFormattedDatetime =
     return (locals) => {
       this.interceptedOnChange = locals.onChange;
       return (
         <div className="form-group">
           <Datetime value={locals.value} onChange={this.interceptorOnChange.bind(this)} inputProps={this.inputProps} />
-          {this.props.inputProps}
+          { (this.state.UTCRenderedDateTime) ?
+            (<div>
+              <i>{`UTC datetime: ${this.state.UTCRenderedDateTime.format("dddd, MMMM Do YYYY, h:mm:ss a")}`}</i>
+            </div>)
+            : ''
+          }
+          <div className="help-block margin-vertical-xs"><i>Insert datetime in your local time format.</i></div>
         </div>
       );
     };
   };
 
   interceptorOnChange (val) {
-    console.log('changed val:', val.toDate());
+    //console.log('changed val:', val.format("dddd, MMMM Do YYYY, h:mm:ss a"));
+    this.setState({ UTCRenderedDateTime: val.clone().utc() });
+    //console.log('changed UTC val:', val.format("dddd, MMMM Do YYYY, h:mm:ss a"));
     this.interceptedOnChange(val.toDate());
   };
 
