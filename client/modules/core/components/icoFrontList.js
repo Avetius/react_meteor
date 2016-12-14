f5f95d231356acbd2f7540a8ab97769b55f19ce5
Jindrich Bartek <jindrich.bartek@gmail.com>
2016-12-14 02:41:38 +0100

    fix nav bar, add tabs for different ICO types

--- client/modules/core/components/icoFrontList.js
+++ client/modules/core/components/icoFrontList.js
@@ -1,16 +1,79 @@
-import { ListGroup } from '@sketchpixy/rubix';
+import { ListGroup, Tabs, Tab } from '@sketchpixy/rubix';
 import IcoShortItem from './icoShortItem';
+import IcoStatus from '/lib/icoStatus';
 
-const IcoFrontList = (props) => {
+import _ from 'lodash';
 
-  return (
-    <ListGroup componentClass="ul">
-      {
-        props.icoEntities.map((icoEntity) => {
-        return <IcoShortItem key={icoEntity._id} icoEntity={icoEntity} />
-      })}
-    </ListGroup>
-  );
-};
+export default class IcoFrontList extends React.Component  {
 
-export default IcoFrontList;
+  constructor (props) {
+    super(props);
+    this.state = {
+      activeTab: 'ongoing'
+    };
+  }
+
+  getOngoingIcoEntities () {
+    return _.filter(this.props.icoEntities, IcoStatus.isOngoing);
+  }
+
+  getUpcomingIcoEntities () {
+    return _.filter(this.props.icoEntities, IcoStatus.isUpcoming);
+  }
+
+  getFinishedIcoEntities () {
+    return _.filter(this.props.icoEntities, IcoStatus.isFinished);
+  }
+
+  handleSelect(selectedKey) {
+    this.setState({ activeTab: selectedKey });
+  }
+
+  render () {
+    return (
+      <div className="margin-top-md">
+
+        <Tabs activeKey={this.state.activeTab} onSelect={this.handleSelect.bind(this)} id="controlled-tab-example">
+
+          <Tab eventKey={'ongoing'} title="Ongoing">
+
+            <h3>Ongoing</h3>
+            <ListGroup componentClass="ul">
+              {
+                this.getOngoingIcoEntities().map((icoEntity) => {
+                  return <IcoShortItem key={icoEntity._id} icoEntity={icoEntity} />
+                })}
+            </ListGroup>
+
+          </Tab>
+
+          <Tab eventKey={'upcoming'} title="Upcoming">
+
+            <h3>Upcoming</h3>
+            <ListGroup componentClass="ul">
+              {
+                this.getUpcomingIcoEntities().map((icoEntity) => {
+                  return <IcoShortItem key={icoEntity._id} icoEntity={icoEntity} />
+                })}
+            </ListGroup>
+
+          </Tab>
+
+          <Tab eventKey={'finished'} title="Finished">
+
+            <h3>Finished</h3>
+            <ListGroup componentClass="ul">
+              {
+                this.getFinishedIcoEntities().map((icoEntity) => {
+                  return <IcoShortItem key={icoEntity._id} icoEntity={icoEntity} />
+                })}
+            </ListGroup>
+
+          </Tab>
+        </Tabs>
+
+      </div>
+    );
+  }
+
+}
--- client/modules/core/components/navigation.js
+++ client/modules/core/components/navigation.js
@@ -2,20 +2,32 @@ import React from 'react';
 // Rubix theme
 import { Nav, NavItem } from '@sketchpixy/rubix';
 
-const navInstance = () => (
-  <Nav bsStyle="pills" className='nav-orange75 padding-vertical-sm padding-horizontal-sm content-elem-bg-color'
-       activeKey={1} onSelect={handleSelect}>
-    <NavItem eventKey={1} href="/">Home</NavItem>
-    <NavItem eventKey={2} href="#">Favourite</NavItem>
-    <NavItem eventKey={3} href="/admin/edit-ico/41b7cfcc-fb0b-4350-a549-b656370fb079">Edit (tmp)</NavItem>
-    <NavItem eventKey={3} href="/admin/edit-ico/31b7cfcc-fb0b-4350-a549-b656370fb079">NotFound (tmp)</NavItem>
-    <NavItem eventKey={4} href="/admin/add-ico">Add</NavItem>
+class Navigation extends React.Component {
 
-  </Nav>
-);
+  constructor (props) {
+    super(props);
+    this.state = {
+      activeKey: 'ico.home'
+    };
+  }
 
-function handleSelect(selectedKey) {
-  console.log('selected ', selectedKey);
+  handleSelect(selectedKey) {
+    this.setState({ activeKey: selectedKey });
+  }
+
+  render () {
+   return  (
+     <Nav bsStyle="pills" justified className='nav-orange75 padding-vertical-sm padding-horizontal-sm content-elem-bg-color'
+          activeKey={this.state.activeKey} onSelect={this.handleSelect.bind(this)}>
+       <NavItem eventKey={'ico.home'} href="/">Home</NavItem>
+       <NavItem eventKey={'ico.favourites'} href="#">Favourite</NavItem>
+       <NavItem eventKey={'ico.edit'} href="/admin/edit-ico/41b7cfcc-fb0b-4350-a549-b656370fb079">Edit (tmp)</NavItem>
+       <NavItem eventKey={'404'} href="/admin/edit-ico/31b7cfcc-fb0b-4350-a549-b656370fb079">NotFound (tmp)</NavItem>
+       <NavItem eventKey={'ico.add'} href="/admin/add-ico">Add</NavItem>
+
+     </Nav>
+   );
+  }
 }
 
- export default navInstance;
+export default Navigation;
