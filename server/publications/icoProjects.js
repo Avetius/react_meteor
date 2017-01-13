0919d6f2c962855d41155b43d9df609186d84a5b
Jindrich Bartek <jindrich.bartek@gmail.com>
2017-01-13 01:39:56 +0100

    disable hiding of entityState field for non-admins, consider more testing this

--- server/publications/icoProjects.js
+++ server/publications/icoProjects.js
@@ -20,7 +20,8 @@ export default function () {
       $or: [{ 'entityState.state': 'published' }, { 'entityState.state': 'concept' }] };
 
     if (!this.userId) {
-      options.fields = {...options.fields, entityState: 0};
+      // todo make some omits for admin data for non-admins?
+      //options.fields = {...options.fields};
     }
 
     return IcoProjects.find(selector, options);
