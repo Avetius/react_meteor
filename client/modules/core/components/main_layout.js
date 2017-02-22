ec5a036a83fc835b8a570594e678181f4d28197e
Jindrich Bartek <jindrich.bartek@gmail.com>
2017-02-22 18:23:27 +0100

    feat(styles): category nav reworked, add stickyness

--- client/modules/core/components/main_layout.js
+++ client/modules/core/components/main_layout.js
@@ -6,48 +6,55 @@ import * as Rb from 'react-bootstrap';
 import {Accounts} from 'meteor/std:accounts-ui';
 
 import { Grid, Row, Col, MainContainer } from '@sketchpixy/rubix';
+import {StickyContainer} from 'react-sticky';
 
-const Layout = ({content = () => null, context}) => {
+class Layout extends React.Component {
 
-  return (
-    <div className="">
+  render () {
 
-      <header className="fg-white">
-        <div className="container-fluid">
+    return (
+      <div>
+        <StickyContainer>
+
+          <header className="fg-white">
+            <div className="container-fluid">
               <Navigation/>
-        </div>
-      </header>
+            </div>
+          </header>
 
-      <div className="container-fluid min-height-51rem margin-vertical-md">
-        <div className="row">
-          <div className="col-md-10 col-md-push-1">
-            {content()}
-          </div>
-        </div>
-      </div>
-
-      { /* todo: move it to separate cmp */ }
-      <footer>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-12 col-md-10 col-md-push-1">
-
-              <div className="row">
-                <div className="col-xs-12 col-md-5">
-                  IcoIndex.com
-                </div>
-                <div className="col-xs-12 col-md-5">
-                  <a className="hidden-link" href={FlowRouter.path('login')}>Login</a>
-                </div>
+          <div className="container-fluid min-height-51rem margin-vertical-md">
+            <div className="row">
+              <div className="col-md-10 col-md-push-1">
+                {this.props.content()}
               </div>
-
             </div>
           </div>
-        </div>
-      </footer>
 
-    </div>
-  );
-};
+          { /* todo: move it to separate cmp */ }
+          <footer>
+            <div className="container-fluid">
+              <div className="row">
+                <div className="col-xs-12 col-md-10 col-md-push-1">
+
+                  <div className="row">
+                    <div className="col-xs-12 col-md-5">
+                      IcoIndex.com
+                    </div>
+                    <div className="col-xs-12 col-md-5">
+                      <a className="hidden-link" href={FlowRouter.path('login')}>Login</a>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+          </footer>
+
+        </StickyContainer>
+      </div>
+    );
+  }
+
+}
 
 export default Layout;
--- client/modules/core/components/navigation.js
+++ client/modules/core/components/navigation.js
@@ -4,10 +4,17 @@ import { Tab, Row, Col, Nav, NavItem } from '@sketchpixy/rubix';
 import {Meteor} from 'meteor/meteor';
 import AccountsMgmt from '/lib/accountsMgmt';
 import {FlowRouter} from 'meteor/kadira:flow-router';
+import {Sticky} from 'react-sticky';
 
 class Navigation extends React.Component {
 
+  // todo remove:
+  onSticky () {
+    console.log('stickOn');
+  }
+
   render () {
+
     let publishedCountStr, conceptsCountStr;
     if (!this.props.globalCounts) {
       publishedCountStr = conceptsCountStr = '';
@@ -18,9 +25,9 @@ class Navigation extends React.Component {
       conceptsCountStr = (!publishedCount && publishedCount !== 0) ? '' :` (${conceptsCount})`;
     }
 
-    let mainNavigation;
+    let userUtilNav;
     if (AccountsMgmt.isCurrentUserAdmin()) {
-      mainNavigation =
+      userUtilNav =
         <Nav bsStyle="pills"
              id="user-util-nav"
              className='nav-theme-orange padding-vertical-sm padding-horizontal-sm'
@@ -44,7 +51,7 @@ class Navigation extends React.Component {
           </NavItem>
         </Nav>
     } else {
-      mainNavigation =
+      userUtilNav =
         <Nav bsStyle="pills" className='nav-theme-orange padding-vertical-sm padding-horizontal-sm'
            id="user-util-nav"
            activeKey={this.props.view}>
@@ -59,8 +66,8 @@ class Navigation extends React.Component {
         </Nav>;
     }
 
-    const mainFixedNavPanel =
-      <div className="row main-fixed-nav-panel full-width bg-theme-darkblue">
+    const topHeaderPanel =
+      <div className="row full-width bg-theme-darkblue">
         <div className="col-xs-12 col-md-10 col-md-push-1">
 
           <div className="row row-vertical-center">
@@ -68,64 +75,61 @@ class Navigation extends React.Component {
               <img className="img-responsive" src="/ICOindex.com-white.png" />
             </div>
             <div className="col-xs-10">
-              {mainNavigation}
+              {userUtilNav}
             </div>
           </div>
 
         </div>
       </div>;
 
-    const placeholderForFixedPanel =
-      <div className="main-fixed-nav-panel-placeholder"> </div>;
-
 
     let subNavigationPanel;
     if (this.props.subNav) {
 
       subNavigationPanel =
-        <div className="row bg-primary-gradient">
-          <div className="col-xs-12 col-md-10 col-md-push-1">
-
-            <div className="row">
-              <div className="col-md-10 col-md-push-1">
-                <Nav bsStyle="tabs"
-                     className="padding-horizontal-sm margin-bottom-negative-1"
-                     id="category-nav"
-                     activeKey={this.props.subNav.subView}>
-                  <NavItem className="" eventKey={'ongoing'} href={FlowRouter.path(this.props.subNav.view, { subView: 'ongoing' })}>
-                    <strong>Ongoing</strong>
-                    <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.ongoing }</span>
-                  </NavItem>
-                  <NavItem eventKey={'upcoming'} href={FlowRouter.path(this.props.subNav.view, { subView: 'upcoming' })}>
-                    <strong>Upcoming</strong>
-                    <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.upcoming }</span>
-                  </NavItem>
-                  <NavItem eventKey={'finished'} href={FlowRouter.path(this.props.subNav.view, { subView: 'finished' })}>
-                    <strong>Finished</strong>
-                    <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.finished }</span>
-                  </NavItem>
-                  <NavItem className="pull-right" eventKey={'scam'} href={FlowRouter.path(this.props.subNav.view, { subView: 'scam' })}>
-                    <em>Scam</em>
-                    <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.scam }</span>
-                  </NavItem>
-                  <NavItem className="pull-right" eventKey={'suspicious'} href={FlowRouter.path(this.props.subNav.view, { subView: 'suspicious' })}>
-                    <em>Suspicious</em>
-                    <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.suspicious }</span>
-                  </NavItem>
-                </Nav>
-              </div>
+        <Sticky id="category-nav-wrapper" stickyClassName="sticked" onStickyStateChange={this.onSticky}>
+          <div className="row bg-primary-gradient row-vertical-center">
+            <div className="col-md-2">
+              <img className="ico-logo" src="/ICOindex.com-white.png" />
             </div>
+            <div className="col-xs-12 col-md-9">
 
+              <Nav bsStyle="tabs"
+                   className="padding-horizontal-sm margin-bottom-negative-1"
+                   id="category-nav"
+                   activeKey={this.props.subNav.subView}>
+                <NavItem className="" eventKey={'ongoing'} href={FlowRouter.path(this.props.subNav.view, { subView: 'ongoing' })}>
+                  <strong>Ongoing</strong>
+                  <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.ongoing }</span>
+                </NavItem>
+                <NavItem eventKey={'upcoming'} href={FlowRouter.path(this.props.subNav.view, { subView: 'upcoming' })}>
+                  <strong>Upcoming</strong>
+                  <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.upcoming }</span>
+                </NavItem>
+                <NavItem eventKey={'finished'} href={FlowRouter.path(this.props.subNav.view, { subView: 'finished' })}>
+                  <strong>Finished</strong>
+                  <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.finished }</span>
+                </NavItem>
+                <NavItem className="pull-right" eventKey={'scam'} href={FlowRouter.path(this.props.subNav.view, { subView: 'scam' })}>
+                  <em>Scam</em>
+                  <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.scam }</span>
+                </NavItem>
+                <NavItem className="pull-right" eventKey={'suspicious'} href={FlowRouter.path(this.props.subNav.view, { subView: 'suspicious' })}>
+                  <em>Suspicious</em>
+                  <span className="badge badge-ico margin-left-md">{ this.props.subNav.categoryCounts.suspicious }</span>
+                </NavItem>
+              </Nav>
+
+            </div>
           </div>
-        </div>;
+        </Sticky>;
     }
 
 
 
     return (
       <div>
-        {placeholderForFixedPanel}
-        {mainFixedNavPanel}
+        {topHeaderPanel}
         {subNavigationPanel}
       </div>
     );
