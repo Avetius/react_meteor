0ba3aa6ffaaefd0fea42e755502e695541a99b38
Jindrich Bartek <jindrich.bartek@gmail.com>
2017-03-23 02:19:32 +0100

    feat(sticky): sticky comp: remove unused parts and comments

--- client/modules/core/components/sticky.js
+++ client/modules/core/components/sticky.js
@@ -3,8 +3,7 @@ import React from 'react';
 export default class Sticky extends React.Component {
 
   scrollHandler () {
-    // todo: deferredComputeTopPos feature not needed now
-    if (!this.stickyElem || (this.props.deferredComputeTopPos && !this.props.deferredComputeTopPos.now)) {
+    if (!this.stickyElem) {
       return;
     }
 
@@ -19,10 +18,6 @@ export default class Sticky extends React.Component {
       stickyClasses = ['sticked'];
     }
 
-    if (this.props.name === 'nav-bar') {
-      console.log(scrollTop, this.stickyElemTopPos + deltaY);
-    }
-
     if (scrollTop >= this.stickyElemTopPos + deltaY) {
       stickyClasses.forEach(className => {
         this.stickyElem.classList.add(className);
@@ -35,30 +30,8 @@ export default class Sticky extends React.Component {
     }
   }
 
-  // todo: deferredComputeTopPos feature not needed now
-  componentDidUpdate(prevProps) {
-    if (this.props.name === 'nav-bar') {
-      console.log('received props', ' stickyElemTopPos? ', this.stickyElemTopPos);
-      console.log('this.props..deferredComputeTopPos: ', this.props.deferredComputeTopPos);
-    }
-
-    if ( !this.stickyElemTopPos && this.stickyElemTopPos !== 0 &&
-      this.props.deferredComputeTopPos && this.props.deferredComputeTopPos.now) {
-
-      this.stickyElemTopPos = this.stickyElem.getBoundingClientRect().top;
-      console.log('componentDidUpdate - stickyElemTopPos set to: ', this.stickyElemTopPos);
-    }
-  }
-
   componentDidMount () {
-    // todo: deferredComputeTopPos feature not needed now
-    if (!this.props.deferredComputeTopPos) {
-      this.stickyElemTopPos = this.stickyElem.getBoundingClientRect().top;
-      if (this.props.name === 'nav-bar') {
-        console.log('did mount and stickyElemTopPos set: ', this.stickyElem.getBoundingClientRect().top);
-      }
-
-    }
+    this.stickyElemTopPos = this.stickyElem.getBoundingClientRect().top;
     document.addEventListener('scroll', this.scrollHandler.bind(this));
   }
 
